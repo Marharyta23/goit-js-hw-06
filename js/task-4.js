c3de670f0891893c9a9354aa1bfe8d472c488a49
Marharyta23 <144152456+Marharyta23@users.noreply.github.com>
2023-12-19 23:13:36 +0200

    done

--- js/task-4.js
+++
@@ -1,6 +0,0 @@
-const getTotalBalanceByGender = (users, gender) =>
-  users
-    .filter((sex) => sex.gender === gender)
-    .reduce((total, user) => {
-      return total + user.balance;
-    }, 0);
